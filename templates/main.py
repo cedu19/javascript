dc4d049b19e2436c5bae1fd95d648708bcd415ff
cedu19 <loafparty19@gmail.com>
2026-08-14 13:02:19 +0300

    first commit

--- templates/main.py
+++ templates/main.py
@@ -2,15 +2,15 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
-app.route("/")
+@app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("base.html")
 
-app.route("/my_task")
+@app.route("/my_tax")
 def my_task():
-    return render_template("my_task.html")
+    return render_template("my_tax.html")
 
-app.route("/itax")
+@app.route("/itax")
 def itax():
     return render_template("itax.html")
 
